418b81223f3859d9bc427357176fa7a1e8691216
Betinsk <gmdust@hotmail.com>
2022-04-04 09:57:31 -0300

    doWhile

---
+++ doWhile.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Do</title>
+</head>
+<body>
+
+	<?php 
+
+	$x = 10;
+
+	do {
+		echo 'Entrou no do while';
+	} while ($x < 9);
+		
+
+
+	echo '<br />';
+
+	while ($x < 9) {
+		echo 'Entrou no do while';
+	}
+	 ?>
+
+</body>
+</html>
