6dc3d75f3b1b0991b2cb5e9ab9d09ba43aa8334b
Betinsk <gmdust@hotmail.com>
2022-04-04 10:04:51 -0300

    For

---
+++ for.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>For</title>
+</head>
+<body>
+	<?php 
+
+	$array = ['Banana', 'Maça', 'Uva'];
+
+	for ($i=0; $i < 3; $i++) { 
+		echo '<pre>';
+		print_r($array);
+	}
+
+
+
+
+
+
+
+
+	 ?>
+</body>
+</html>
